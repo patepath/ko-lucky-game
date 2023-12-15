dcb06d4cdcaceefaf33e09611666bf22354b55c5
patepath <patepath@gmail.com>
2023-12-15 22:40:38 +0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CheckinComponent } from './checkin/checkin.component';
+
+const routes: Routes = [
+  { path: 'checkin', component: CheckinComponent },
+  { path: '', redirectTo: 'checkin', pathMatch: 'full' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
