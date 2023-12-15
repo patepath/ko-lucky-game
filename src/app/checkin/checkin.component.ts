e6c82943d868db252f65b043b6f7eed1a9e345f3
patepath <patepath@gmail.com>
2023-12-16 00:28:23 +0700

    Arrange basic layout

--- src/app/checkin/checkin.component.ts
+++ src/app/checkin/checkin.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-checkin',
   templateUrl: './checkin.component.html',
   styleUrls: ['./checkin.component.css']
 })
-export class CheckinComponent implements OnInit {
+export class CheckinComponent implements OnInit, AfterViewInit {
+  @ViewChild('txtqrcode')
+  txtqrcode!: ElementRef<HTMLInputElement>;
+
+  @ViewChild('txtsearch')
+  txtsearch!: ElementRef<HTMLInputElement>;
+
+  public status: number = 1;
+  public qrcode$ = new Subject<string>;
 
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.qrcode$
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe(s => {
+        console.log(s);
+      });
   }
 
-  search() {
-    
+  ngAfterViewInit(): void {
+  }
+
+  doQRCode(code: string) {
+    this.qrcode$.next(code);
+  }
+
+  doSearch() {
+    this.status = 2;
   }
 }
